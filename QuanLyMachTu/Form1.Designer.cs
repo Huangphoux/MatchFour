451e1226166eefbc2f099df3812b9184e40547ed
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2024-10-26 08:39:30 +0700

    Set codename to MatchFour

--- QuanLyMachTu/Form1.Designer.cs
+++ QuanLyMachTu/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuanLyMachTu
 {
-    partial class Form1
+    partial class MatchFour
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MatchFour));
+            flatTabControl1 = new DarkModeForms.FlatTabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            flatTabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flatTabControl1
+            // 
+            resources.ApplyResources(flatTabControl1, "flatTabControl1");
+            flatTabControl1.BorderColor = SystemColors.ControlDark;
+            flatTabControl1.Controls.Add(tabPage1);
+            flatTabControl1.Controls.Add(tabPage2);
+            flatTabControl1.DrawMode = TabDrawMode.OwnerDrawFixed;
+            flatTabControl1.LineColor = SystemColors.Highlight;
+            flatTabControl1.Multiline = true;
+            flatTabControl1.Name = "flatTabControl1";
+            flatTabControl1.SelectedForeColor = SystemColors.HighlightText;
+            flatTabControl1.SelectedIndex = 0;
+            flatTabControl1.SelectTabColor = SystemColors.ControlLight;
+            flatTabControl1.ShowTabCloseButton = false;
+            flatTabControl1.SizeMode = TabSizeMode.Fixed;
+            flatTabControl1.TabCloseColor = SystemColors.ControlText;
+            flatTabControl1.TabColor = SystemColors.ControlLight;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = SystemColors.ControlLight;
+            resources.ApplyResources(tabPage1, "tabPage1");
+            tabPage1.Name = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = SystemColors.ControlLight;
+            resources.ApplyResources(tabPage2, "tabPage2");
+            tabPage2.Name = "tabPage2";
+            // 
+            // MatchFour
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(flatTabControl1);
+            Name = "MatchFour";
+            flatTabControl1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DarkModeForms.FlatTabControl flatTabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
