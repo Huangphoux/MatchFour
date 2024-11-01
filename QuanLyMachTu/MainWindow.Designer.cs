af8435cd7b0826f9b1bec4de54d91bda705cd8ba
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2024-11-01 19:56:51 +0700

    Add pressed state for button, change Home to Overview, add Home button

--- QuanLyMachTu/MainWindow.Designer.cs
+++ QuanLyMachTu/MainWindow.Designer.cs
@@ -34,9 +34,12 @@
             iconButton_TaiChinh = new FontAwesome.Sharp.IconButton();
             iconButton_QuanLy = new FontAwesome.Sharp.IconButton();
             iconButton_LichHen = new FontAwesome.Sharp.IconButton();
-            iconButton_TrangChu = new FontAwesome.Sharp.IconButton();
+            iconButton_TomTat = new FontAwesome.Sharp.IconButton();
             panel1 = new Panel();
+            iconPictureBox_Home = new FontAwesome.Sharp.IconPictureBox();
             panel_Menu.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox_Home).BeginInit();
             SuspendLayout();
             // 
             // panel_Menu
@@ -47,86 +50,105 @@
             panel_Menu.Controls.Add(iconButton_TaiChinh);
             panel_Menu.Controls.Add(iconButton_QuanLy);
             panel_Menu.Controls.Add(iconButton_LichHen);
-            panel_Menu.Controls.Add(iconButton_TrangChu);
+            panel_Menu.Controls.Add(iconButton_TomTat);
             panel_Menu.Controls.Add(panel1);
             resources.ApplyResources(panel_Menu, "panel_Menu");
             panel_Menu.Name = "panel_Menu";
             // 
             // iconButton_CaiDat
             // 
+            iconButton_CaiDat.BackColor = Color.White;
             resources.ApplyResources(iconButton_CaiDat, "iconButton_CaiDat");
             iconButton_CaiDat.FlatAppearance.BorderColor = Color.Silver;
-            iconButton_CaiDat.FlatAppearance.MouseDownBackColor = Color.Turquoise;
-            iconButton_CaiDat.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
+            iconButton_CaiDat.FlatAppearance.BorderSize = 0;
             iconButton_CaiDat.ForeColor = Color.Black;
             iconButton_CaiDat.IconChar = FontAwesome.Sharp.IconChar.Cog;
             iconButton_CaiDat.IconColor = Color.Black;
             iconButton_CaiDat.IconFont = FontAwesome.Sharp.IconFont.Auto;
             iconButton_CaiDat.IconSize = 50;
             iconButton_CaiDat.Name = "iconButton_CaiDat";
-            iconButton_CaiDat.UseVisualStyleBackColor = true;
+            iconButton_CaiDat.UseVisualStyleBackColor = false;
+            iconButton_CaiDat.Click += iconButton_CaiDat_Click;
             // 
             // iconButton_TaiChinh
             // 
+            iconButton_TaiChinh.BackColor = Color.White;
             resources.ApplyResources(iconButton_TaiChinh, "iconButton_TaiChinh");
             iconButton_TaiChinh.FlatAppearance.BorderColor = Color.Silver;
-            iconButton_TaiChinh.FlatAppearance.MouseDownBackColor = Color.Turquoise;
-            iconButton_TaiChinh.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
+            iconButton_TaiChinh.FlatAppearance.BorderSize = 0;
             iconButton_TaiChinh.ForeColor = Color.Black;
             iconButton_TaiChinh.IconChar = FontAwesome.Sharp.IconChar.SackDollar;
             iconButton_TaiChinh.IconColor = Color.Black;
             iconButton_TaiChinh.IconFont = FontAwesome.Sharp.IconFont.Auto;
             iconButton_TaiChinh.IconSize = 50;
             iconButton_TaiChinh.Name = "iconButton_TaiChinh";
-            iconButton_TaiChinh.UseVisualStyleBackColor = true;
+            iconButton_TaiChinh.UseVisualStyleBackColor = false;
+            iconButton_TaiChinh.Click += iconButton_TaiChinh_Click;
             // 
             // iconButton_QuanLy
             // 
+            iconButton_QuanLy.BackColor = Color.White;
             resources.ApplyResources(iconButton_QuanLy, "iconButton_QuanLy");
             iconButton_QuanLy.FlatAppearance.BorderColor = Color.Silver;
-            iconButton_QuanLy.FlatAppearance.MouseDownBackColor = Color.Turquoise;
-            iconButton_QuanLy.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
+            iconButton_QuanLy.FlatAppearance.BorderSize = 0;
             iconButton_QuanLy.ForeColor = Color.Black;
             iconButton_QuanLy.IconChar = FontAwesome.Sharp.IconChar.CircleInfo;
             iconButton_QuanLy.IconColor = Color.Black;
             iconButton_QuanLy.IconFont = FontAwesome.Sharp.IconFont.Auto;
             iconButton_QuanLy.IconSize = 50;
             iconButton_QuanLy.Name = "iconButton_QuanLy";
-            iconButton_QuanLy.UseVisualStyleBackColor = true;
+            iconButton_QuanLy.UseVisualStyleBackColor = false;
+            iconButton_QuanLy.Click += iconButton_QuanLy_Click;
             // 
             // iconButton_LichHen
             // 
+            iconButton_LichHen.BackColor = Color.White;
             resources.ApplyResources(iconButton_LichHen, "iconButton_LichHen");
             iconButton_LichHen.FlatAppearance.BorderColor = Color.Silver;
-            iconButton_LichHen.FlatAppearance.MouseDownBackColor = Color.Turquoise;
-            iconButton_LichHen.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
+            iconButton_LichHen.FlatAppearance.BorderSize = 0;
             iconButton_LichHen.ForeColor = Color.Black;
-            iconButton_LichHen.IconChar = FontAwesome.Sharp.IconChar.CalendarCheck;
+            iconButton_LichHen.IconChar = FontAwesome.Sharp.IconChar.CalendarWeek;
             iconButton_LichHen.IconColor = Color.Black;
             iconButton_LichHen.IconFont = FontAwesome.Sharp.IconFont.Auto;
             iconButton_LichHen.IconSize = 50;
             iconButton_LichHen.Name = "iconButton_LichHen";
-            iconButton_LichHen.UseVisualStyleBackColor = true;
+            iconButton_LichHen.UseVisualStyleBackColor = false;
+            iconButton_LichHen.Click += iconButton_LichHen_Click;
             // 
-            // iconButton_TrangChu
+            // iconButton_TomTat
             // 
-            resources.ApplyResources(iconButton_TrangChu, "iconButton_TrangChu");
-            iconButton_TrangChu.FlatAppearance.BorderColor = Color.Silver;
-            iconButton_TrangChu.FlatAppearance.MouseDownBackColor = Color.Turquoise;
-            iconButton_TrangChu.FlatAppearance.MouseOverBackColor = Color.PaleTurquoise;
-            iconButton_TrangChu.ForeColor = Color.Black;
-            iconButton_TrangChu.IconChar = FontAwesome.Sharp.IconChar.House;
-            iconButton_TrangChu.IconColor = Color.Black;
-            iconButton_TrangChu.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconButton_TrangChu.IconSize = 50;
-            iconButton_TrangChu.Name = "iconButton_TrangChu";
-            iconButton_TrangChu.UseVisualStyleBackColor = true;
+            iconButton_TomTat.BackColor = Color.White;
+            resources.ApplyResources(iconButton_TomTat, "iconButton_TomTat");
+            iconButton_TomTat.FlatAppearance.BorderColor = Color.Silver;
+            iconButton_TomTat.FlatAppearance.BorderSize = 0;
+            iconButton_TomTat.ForeColor = Color.Black;
+            iconButton_TomTat.IconChar = FontAwesome.Sharp.IconChar.ClipboardList;
+            iconButton_TomTat.IconColor = Color.Black;
+            iconButton_TomTat.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton_TomTat.IconSize = 50;
+            iconButton_TomTat.Name = "iconButton_TomTat";
+            iconButton_TomTat.UseVisualStyleBackColor = false;
+            iconButton_TomTat.Click += iconButton_TomTat_Click;
             // 
             // panel1
             // 
             panel1.BackColor = Color.LightGray;
+            panel1.Controls.Add(iconPictureBox_Home);
             resources.ApplyResources(panel1, "panel1");
             panel1.Name = "panel1";
+            // 
+            // iconPictureBox_Home
+            // 
+            iconPictureBox_Home.BackColor = Color.LightGray;
+            iconPictureBox_Home.ForeColor = SystemColors.ControlText;
+            iconPictureBox_Home.IconChar = FontAwesome.Sharp.IconChar.ClinicMedical;
+            iconPictureBox_Home.IconColor = SystemColors.ControlText;
+            iconPictureBox_Home.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconPictureBox_Home.IconSize = 120;
+            resources.ApplyResources(iconPictureBox_Home, "iconPictureBox_Home");
+            iconPictureBox_Home.Name = "iconPictureBox_Home";
+            iconPictureBox_Home.TabStop = false;
+            iconPictureBox_Home.Click += iconPictureBox1_Click;
             // 
             // MainWindow
             // 
@@ -136,17 +158,20 @@
             Controls.Add(panel_Menu);
             Name = "MainWindow";
             panel_Menu.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)iconPictureBox_Home).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel_Menu;
-        private FontAwesome.Sharp.IconButton iconButton_TrangChu;
+        private FontAwesome.Sharp.IconButton iconButton_TomTat;
         private Panel panel1;
         private FontAwesome.Sharp.IconButton iconButton_CaiDat;
         private FontAwesome.Sharp.IconButton iconButton_TaiChinh;
         private FontAwesome.Sharp.IconButton iconButton_LichHen;
         private FontAwesome.Sharp.IconButton iconButton_QuanLy;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox_Home;
     }
 }
--- QuanLyMachTu/MainWindow.cs
+++ QuanLyMachTu/MainWindow.cs
@@ -4,9 +4,114 @@ namespace QuanLyMachTu
 {
     public partial class MainWindow : Form
     {
+        private IconButton currentButton;
+        private readonly Panel leftBorderButton;
+        private readonly Form currentChildForm;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            leftBorderButton = new Panel
+            {
+                Size = new Size(10, 80)
+            };
+
+            panel_Menu.Controls.Add(leftBorderButton);
+
+            //Form
+            // Text = string.Empty;
+            // this.ControlBox = false;
+
+            DoubleBuffered = true;
+            MaximizedBounds = Screen.FromHandle(Handle).WorkingArea;
+        }
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+
+        //Methods
+        private void ActivateButton(object senderButton, Color color)
+        {
+            if (senderButton != null)
+            {
+                DisableButton();
+
+                //Button
+                currentButton = (IconButton)senderButton;
+
+                currentButton.ForeColor = color;
+                currentButton.BackColor = Color.White;
+                currentButton.TextAlign = ContentAlignment.MiddleCenter;
+                currentButton.IconColor = color;
+                //currentButton.TextImageRelation = TextImageRelation.TextBeforeImage;
+                //currentButton.ImageAlign = ContentAlignment.MiddleRight;
+
+                //Left border button
+                leftBorderButton.BackColor = color;
+                leftBorderButton.Location = new Point(0, currentButton.Location.Y);
+                leftBorderButton.Visible = true;
+                leftBorderButton.BringToFront();
+
+                //Current Child Form Icon
+                //iconCurrentChildForm.IconChar = currentButton.IconChar;
+                //iconCurrentChildForm.IconColor = color;
+            }
+        }
+
+        private void DisableButton()
+        {
+            if (currentButton != null)
+            {
+                currentButton.ForeColor = Color.Black;
+                currentButton.BackColor = Color.White;
+                currentButton.TextAlign = ContentAlignment.MiddleCenter;
+                currentButton.IconColor = Color.Black;
+                //currentButton.TextImageRelation = TextImageRelation.ImageBeforeText;
+                //currentButton.ImageAlign = ContentAlignment.MiddleLeft;
+            }
+        }
+
+        private void iconButton_TomTat_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void iconButton_LichHen_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void iconButton_QuanLy_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+
+        private void iconButton_TaiChinh_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+        }
+
+        private void iconButton_CaiDat_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color5);
+        }
+
+        private void iconPictureBox1_Click(object sender, EventArgs e)
+        {
+            DisableButton();
+            leftBorderButton.Visible = false;
+
+            //iconCurrentChildForm.IconChar = IconChar.Home;
+            //iconCurrentChildForm.IconColor = Color.MediumPurple;
+            // lblTitleChildForm.Text = "Home";
         }
     }
 }
